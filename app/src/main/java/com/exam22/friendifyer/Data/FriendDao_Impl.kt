6fd4ed03041bbd4f4f48da30b6a939f48cc03807
WackyGlow <stampe12@gmail.com>
2022-05-13 13:06:50 +0200

    Update FriendDao_Impl.kt

--- app/src/main/java/com/exam22/friendifyer/Data/FriendDao_Impl.kt
+++ app/src/main/java/com/exam22/friendifyer/Data/FriendDao_Impl.kt
@@ -4,35 +4,35 @@ import com.exam22.friendifyer.models.Friend
 
 class FriendDao_Impl : IFriendDao {
     override fun getAll(): Array<BeFriend> {
-        
+        TODO()
     }
 
     override fun getByName(s: String): Array<BeFriend> {
-
+        TODO()
     }
 
     override fun getAllNames(): Array<String> {
-
+        TODO()
     }
 
     override fun getById(id: Int): BeFriend {
-
+        TODO()
     }
 
     override fun insert(f: BeFriend) {
-
+        TODO()
     }
 
     override fun update(f: BeFriend) {
-
+        TODO()
     }
 
     override fun delete(f: BeFriend) {
-
+        TODO()
     }
 
     override fun deleteAll() {
-
+        TODO()
     }
 
 }
