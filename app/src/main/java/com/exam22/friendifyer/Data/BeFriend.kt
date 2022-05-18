34947dff8c4eab0efb0b922ddaa67cb384f22550
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2022-05-18 11:24:12 +0200

    Added DB

--- app/src/main/java/com/exam22/friendifyer/Data/BeFriend.kt
+++ app/src/main/java/com/exam22/friendifyer/Data/BeFriend.kt
@@ -1,8 +1,16 @@
 package com.exam22.friendifyer.Data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class BeFriend(
-    var id: Int,
+    @PrimaryKey(autoGenerate = true) var id: Int,
     var name: String,
     var phone: String,
-    var bestFriend: Boolean
-)
+    var bestFriend: Boolean){
+
+    public override fun toString(): String {
+        return "$id: Name: $name, Phone: $phone, IsBestFriend: $bestFriend"
+    }
+}
