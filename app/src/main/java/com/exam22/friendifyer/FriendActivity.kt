ec8e163401b4504adc9d1d75cce976719b908937
HaraldurJoh <zeasick@gmail.com>
2022-05-18 13:15:49 +0200

    lessgo

--- app/src/main/java/com/exam22/friendifyer/FriendActivity.kt
+++ app/src/main/java/com/exam22/friendifyer/FriendActivity.kt
@@ -1,13 +1,16 @@
 package com.exam22.friendifyer
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.view.View
+import com.exam22.friendifyer.Data.BeFriend
 import kotlinx.android.synthetic.main.activity_friend.*
 
 class FriendActivity : AppCompatActivity() {
+    private lateinit var selectFriend: BeFriend
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         print("this is working")
@@ -24,7 +27,22 @@ class FriendActivity : AppCompatActivity() {
                 tf_bestFriend.setText("no")
             }
         }
+        btnCallFriend.setOnClickListener{ onClickCall() }
+        btnSMSFriend.setOnClickListener { onClickSms() }
+    }
 
+    private fun onClickCall() {
+        var number = selectFriend.phone
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$number")
+        startActivity(intent)
+    }
+
+    private fun onClickSms() {
+        val sendIntent = Intent(Intent.ACTION_VIEW)
+        var number = selectFriend.phone
+        sendIntent.data = Uri.parse("sms:$number")
+        startActivity(sendIntent)
     }
 
     fun OnEditClick(view: View) {
